0e2743bc2a3ad1cc90ffb0bd4e475f7ad00a2ab9
William Ennals <pneuviz@gmail.com>
2023-04-09 21:26:59 -0400

    node 15 support update

--- src/schematics/deploy/actions.ts
+++ src/schematics/deploy/actions.ts
@@ -1,10 +1,26 @@
-import { BuilderContext, targetFromTargetString } from '@angular-devkit/architect';
-import { BuildTarget, CloudRunOptions, DeployBuilderSchema, FirebaseTools, FSHost } from '../interfaces';
+import {
+  BuilderContext,
+  Target,
+  targetFromTargetString,
+} from '@angular-devkit/architect';
+import {
+  BuildTarget,
+  CloudRunOptions,
+  DeployBuilderSchema,
+  FirebaseTools,
+  FSHost,
+} from '../interfaces';
 import { existsSync, readFileSync, renameSync, writeFileSync } from 'fs';
 import { copySync, removeSync } from 'fs-extra';
 import { dirname, join } from 'path';
 import { execSync, spawn, SpawnOptionsWithoutStdio } from 'child_process';
-import { defaultFunction, functionGen2, defaultPackage, DEFAULT_FUNCTION_NAME, dockerfile } from './functions-templates';
+import {
+  defaultFunction,
+  functionGen2,
+  defaultPackage,
+  DEFAULT_FUNCTION_NAME,
+  dockerfile,
+} from './functions-templates';
 import { satisfies } from 'semver';
 import open from 'open';
 import { SchematicsException } from '@angular-devkit/schematics';
@@ -56,7 +72,8 @@ const spawnAsync = async (
 
 export type DeployBuilderOptions = DeployBuilderSchema & Record<string, any>;
 
-const escapeRegExp = (str: string) => str.replace(/[\-\[\]\/{}()*+?.\\^$|]/g, '\\$&');
+const escapeRegExp = (str: string) =>
+  str.replace(/[\-\[\]\/{}()*+?.\\^$|]/g, '\\$&');
 
 const moveSync = (src: string, dest: string) => {
   copySync(src, dest);
@@ -68,14 +85,12 @@ const deployToHosting = async (
   context: BuilderContext,
   workspaceRoot: string,
   options: DeployBuilderOptions,
-  firebaseToken?: string,
+  firebaseToken?: string
 ) => {
-
   // tslint:disable-next-line:no-non-null-assertion
   const siteTarget = options.target ?? context.target!.project;
 
   if (options.preview) {
-
     await firebaseTools.serve({
       port: DEFAULT_EMULATOR_PORT,
       host: DEFAULT_EMULATOR_HOST,
@@ -87,11 +102,12 @@ const deployToHosting = async (
     const { deployProject } = await inquirer.prompt({
       type: 'confirm',
       name: 'deployProject',
-      message: 'Would you like to deploy your application to Firebase Hosting?'
+      message: 'Would you like to deploy your application to Firebase Hosting?',
     });
 
-    if (!deployProject) { return; }
-
+    if (!deployProject) {
+      return;
+    }
   }
 
   return await firebaseTools.deploy({
@@ -101,7 +117,6 @@ const deployToHosting = async (
     nonInteractive: true,
     projectRoot: workspaceRoot,
   });
-
 };
 
 const defaultFsHost: FSHost = {
@@ -114,34 +129,53 @@ const defaultFsHost: FSHost = {
 };
 
 const findPackageVersion = (packageManager: string, name: string) => {
-  const match = execSync(`${packageManager} list ${name}`).toString().match(`[^|\s]${escapeRegExp(name)}[@| ][^\s]+(\s.+)?$`);
-  return match ? match[0].split(new RegExp(`${escapeRegExp(name)}[@| ]`))[1].split(/\s/)[0] : null;
+  const match = execSync(`${packageManager} list ${name}`)
+    .toString()
+    .match(`[^|\s]${escapeRegExp(name)}[@| ][^\s]+(\s.+)?$`);
+  return match
+    ? match[0].split(new RegExp(`${escapeRegExp(name)}[@| ]`))[1].split(/\s/)[0]
+    : null;
 };
 
-const getPackageJson = (context: BuilderContext, workspaceRoot: string, options: DeployBuilderOptions, main?: string) => {
+const getPackageJson = (
+  context: BuilderContext,
+  workspaceRoot: string,
+  options: DeployBuilderOptions,
+  main?: string
+) => {
   const dependencies: Record<string, string> = {};
   const devDependencies: Record<string, string> = {};
   if (options.ssr !== 'cloud-run') {
-    Object.keys(firebaseFunctionsDependencies).forEach(name => {
+    Object.keys(firebaseFunctionsDependencies).forEach((name) => {
       const { version, dev } = firebaseFunctionsDependencies[name];
       (dev ? devDependencies : dependencies)[name] = version;
     });
   }
   if (existsSync(join(workspaceRoot, 'angular.json'))) {
-    const angularJson = JSON.parse(readFileSync(join(workspaceRoot, 'angular.json')).toString());
+    const angularJson = JSON.parse(
+      readFileSync(join(workspaceRoot, 'angular.json')).toString()
+    );
     const packageManager = angularJson.cli?.packageManager ?? 'npm';
     // tslint:disable-next-line:no-non-null-assertion
-    const server = angularJson.projects[context.target!.project].architect.server;
+    const server =
+      angularJson.projects[(context.target as Target).project].architect.server;
     const externalDependencies = server?.options?.externalDependencies || [];
     const bundleDependencies = server?.options?.bundleDependencies ?? true;
     if (bundleDependencies) {
-      externalDependencies.forEach(externalDependency => {
-        const packageVersion = findPackageVersion(packageManager, externalDependency);
-        if (packageVersion) { dependencies[externalDependency] = packageVersion; }
+      externalDependencies.forEach((externalDependency) => {
+        const packageVersion = findPackageVersion(
+          packageManager,
+          externalDependency
+        );
+        if (packageVersion) {
+          dependencies[externalDependency] = packageVersion;
+        }
       });
     } else {
       if (existsSync(join(workspaceRoot, 'package.json'))) {
-        const packageJson = JSON.parse(readFileSync(join(workspaceRoot, 'package.json')).toString());
+        const packageJson = JSON.parse(
+          readFileSync(join(workspaceRoot, 'package.json')).toString()
+        );
         Object.keys(packageJson.dependencies).forEach((dependency: string) => {
           dependencies[dependency] = packageJson.dependencies[dependency];
         });
@@ -162,16 +196,25 @@ export const deployToFunction = async (
   firebaseToken?: string,
   fsHost: FSHost = defaultFsHost
 ) => {
-
-  const staticBuildOptions = await context.getTargetOptions(targetFromTargetString(staticBuildTarget.name));
-  if (!staticBuildOptions.outputPath || typeof staticBuildOptions.outputPath !== 'string') {
+  const staticBuildOptions = await context.getTargetOptions(
+    targetFromTargetString(staticBuildTarget.name)
+  );
+  if (
+    !staticBuildOptions.outputPath ||
+    typeof staticBuildOptions.outputPath !== 'string'
+  ) {
     throw new Error(
       `Cannot read the output path option of the Angular project '${staticBuildTarget.name}' in angular.json`
     );
   }
 
-  const serverBuildOptions = await context.getTargetOptions(targetFromTargetString(serverBuildTarget.name));
-  if (!serverBuildOptions.outputPath || typeof serverBuildOptions.outputPath !== 'string') {
+  const serverBuildOptions = await context.getTargetOptions(
+    targetFromTargetString(serverBuildTarget.name)
+  );
+  if (
+    !serverBuildOptions.outputPath ||
+    typeof serverBuildOptions.outputPath !== 'string'
+  ) {
     throw new Error(
       `Cannot read the output path option of the Angular project '${serverBuildTarget.name}' in angular.json`
     );
@@ -180,7 +223,9 @@ export const deployToFunction = async (
   const staticOut = join(workspaceRoot, staticBuildOptions.outputPath);
   const serverOut = join(workspaceRoot, serverBuildOptions.outputPath);
 
-  const functionsOut = options.outputPath ? join(workspaceRoot, options.outputPath) : dirname(serverOut);
+  const functionsOut = options.outputPath
+    ? join(workspaceRoot, options.outputPath)
+    : dirname(serverOut);
   const functionName = options.functionName || DEFAULT_FUNCTION_NAME;
 
   const newStaticOut = join(functionsOut, staticBuildOptions.outputPath);
@@ -229,7 +274,7 @@ export const deployToFunction = async (
         join(newStaticOut, 'index.html'),
         join(newStaticOut, 'index.original.html')
       );
-    } catch (e) { }
+    } catch (e) {}
   }
 
   // tslint:disable-next-line:no-non-null-assertion
@@ -242,7 +287,6 @@ export const deployToFunction = async (
   }
 
   if (options.preview) {
-
     await firebaseTools.serve({
       port: DEFAULT_EMULATOR_PORT,
       host: DEFAULT_EMULATOR_HOST,
@@ -251,13 +295,16 @@ export const deployToFunction = async (
       projectRoot: workspaceRoot,
     });
 
-    const { deployProject} = await inquirer.prompt({
+    const { deployProject } = await inquirer.prompt({
       type: 'confirm',
       name: 'deployProject',
-      message: 'Would you like to deploy your application to Firebase Hosting & Cloud Functions?'
+      message:
+        'Would you like to deploy your application to Firebase Hosting & Cloud Functions?',
     });
 
-    if (!deployProject) { return; }
+    if (!deployProject) {
+      return;
+    }
   }
 
   return await firebaseTools.deploy({
@@ -267,9 +314,7 @@ export const deployToFunction = async (
     nonInteractive: true,
     projectRoot: workspaceRoot,
   });
-
 };
-
 
 export const deployToCloudRun = async (
   firebaseTools: FirebaseTools,
@@ -281,16 +326,25 @@ export const deployToCloudRun = async (
   firebaseToken?: string,
   fsHost: FSHost = defaultFsHost
 ) => {
-
-  const staticBuildOptions = await context.getTargetOptions(targetFromTargetString(staticBuildTarget.name));
-  if (!staticBuildOptions.outputPath || typeof staticBuildOptions.outputPath !== 'string') {
+  const staticBuildOptions = await context.getTargetOptions(
+    targetFromTargetString(staticBuildTarget.name)
+  );
+  if (
+    !staticBuildOptions.outputPath ||
+    typeof staticBuildOptions.outputPath !== 'string'
+  ) {
     throw new Error(
       `Cannot read the output path option of the Angular project '${staticBuildTarget.name}' in angular.json`
     );
   }
 
-  const serverBuildOptions = await context.getTargetOptions(targetFromTargetString(serverBuildTarget.name));
-  if (!serverBuildOptions.outputPath || typeof serverBuildOptions.outputPath !== 'string') {
+  const serverBuildOptions = await context.getTargetOptions(
+    targetFromTargetString(serverBuildTarget.name)
+  );
+  if (
+    !serverBuildOptions.outputPath ||
+    typeof serverBuildOptions.outputPath !== 'string'
+  ) {
     throw new Error(
       `Cannot read the output path option of the Angular project '${serverBuildTarget.name}' in angular.json`
     );
@@ -299,7 +353,9 @@ export const deployToCloudRun = async (
   const staticOut = join(workspaceRoot, staticBuildOptions.outputPath);
   const serverOut = join(workspaceRoot, serverBuildOptions.outputPath);
 
-  const cloudRunOut = options.outputPath ? join(workspaceRoot, options.outputPath) : join(dirname(serverOut), 'run');
+  const cloudRunOut = options.outputPath
+    ? join(workspaceRoot, options.outputPath)
+    : join(dirname(serverOut), 'run');
   const serviceId = options.functionName || DEFAULT_FUNCTION_NAME;
 
   const newStaticOut = join(cloudRunOut, staticBuildOptions.outputPath);
@@ -313,7 +369,12 @@ export const deployToCloudRun = async (
   fsHost.copySync(staticOut, newStaticOut);
   fsHost.copySync(serverOut, newServerOut);
 
-  const packageJson = getPackageJson(context, workspaceRoot, options, join(serverBuildOptions.outputPath, 'main.js'));
+  const packageJson = getPackageJson(
+    context,
+    workspaceRoot,
+    options,
+    join(serverBuildOptions.outputPath, 'main.js')
+  );
   const nodeVersion = packageJson.engines.node;
 
   if (!satisfies(process.versions.node, nodeVersion.toString())) {
@@ -324,13 +385,10 @@ export const deployToCloudRun = async (
 
   fsHost.writeFileSync(
     join(cloudRunOut, 'package.json'),
-    JSON.stringify(packageJson, null, 2),
+    JSON.stringify(packageJson, null, 2)
   );
 
-  fsHost.writeFileSync(
-    join(cloudRunOut, 'Dockerfile'),
-    dockerfile(options)
-  );
+  fsHost.writeFileSync(join(cloudRunOut, 'Dockerfile'), dockerfile(options));
 
   if (!options.prerender) {
     try {
@@ -338,7 +396,7 @@ export const deployToCloudRun = async (
         join(newStaticOut, 'index.html'),
         join(newStaticOut, 'index.original.html')
       );
-    } catch (e) { }
+    } catch (e) {}
   }
 
   if (options.preview) {
@@ -351,19 +409,43 @@ export const deployToCloudRun = async (
     cloudRunOptions[k] ||= v;
   });
   // lean on the schema for validation (rather than sanitize)
-  if (cloudRunOptions.cpus) { deployArguments.push('--cpu', cloudRunOptions.cpus); }
-  if (cloudRunOptions.maxConcurrency) { deployArguments.push('--concurrency', cloudRunOptions.maxConcurrency); }
-  if (cloudRunOptions.maxInstances) { deployArguments.push('--max-instances', cloudRunOptions.maxInstances); }
-  if (cloudRunOptions.memory) { deployArguments.push('--memory', cloudRunOptions.memory); }
-  if (cloudRunOptions.minInstances) { deployArguments.push('--min-instances', cloudRunOptions.minInstances); }
-  if (cloudRunOptions.timeout) { deployArguments.push('--timeout', cloudRunOptions.timeout); }
-  if (cloudRunOptions.vpcConnector) { deployArguments.push('--vpc-connector', cloudRunOptions.vpcConnector); }
+  if (cloudRunOptions.cpus) {
+    deployArguments.push('--cpu', cloudRunOptions.cpus);
+  }
+  if (cloudRunOptions.maxConcurrency) {
+    deployArguments.push('--concurrency', cloudRunOptions.maxConcurrency);
+  }
+  if (cloudRunOptions.maxInstances) {
+    deployArguments.push('--max-instances', cloudRunOptions.maxInstances);
+  }
+  if (cloudRunOptions.memory) {
+    deployArguments.push('--memory', cloudRunOptions.memory);
+  }
+  if (cloudRunOptions.minInstances) {
+    deployArguments.push('--min-instances', cloudRunOptions.minInstances);
+  }
+  if (cloudRunOptions.timeout) {
+    deployArguments.push('--timeout', cloudRunOptions.timeout);
+  }
+  if (cloudRunOptions.vpcConnector) {
+    deployArguments.push('--vpc-connector', cloudRunOptions.vpcConnector);
+  }
 
   // TODO validate serviceId, firebaseProject, and vpcConnector both to limit errors and opp for injection
 
   context.logger.info(`📦 Deploying to Cloud Run`);
-  await spawnAsync(`gcloud builds submit ${cloudRunOut} --tag gcr.io/${options.firebaseProject}/${serviceId} --project ${options.firebaseProject} --quiet`);
-  await spawnAsync(`gcloud run deploy ${serviceId} --image gcr.io/${options.firebaseProject}/${serviceId} --project ${options.firebaseProject} ${deployArguments.join(' ')} --platform managed --allow-unauthenticated --region=${options.region} --quiet`);
+  await spawnAsync(
+    `gcloud builds submit ${cloudRunOut} --tag gcr.io/${options.firebaseProject}/${serviceId} --project ${options.firebaseProject} --quiet`
+  );
+  await spawnAsync(
+    `gcloud run deploy ${serviceId} --image gcr.io/${
+      options.firebaseProject
+    }/${serviceId} --project ${options.firebaseProject} ${deployArguments.join(
+      ' '
+    )} --platform managed --allow-unauthenticated --region=${
+      options.region
+    } --quiet`
+  );
 
   // tslint:disable-next-line:no-non-null-assertion
   const siteTarget = options.target ?? context.target!.project;
@@ -386,24 +468,29 @@ export default async function deploy(
   prerenderBuildTarget: BuildTarget | undefined,
   firebaseProject: string,
   options: DeployBuilderOptions,
-  firebaseToken?: string,
+  firebaseToken?: string
 ) {
-  if (!firebaseToken) {
-    await firebaseTools.login();
-    const user = await firebaseTools.login({ projectRoot: context.workspaceRoot });
+  if (!firebaseToken && !process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    const user = await firebaseTools.login({
+      projectRoot: context.workspaceRoot,
+    });
     console.log(`Logged into Firebase as ${user.email}.`);
   }
 
-  if (prerenderBuildTarget) {
+  if (!firebaseToken && process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    await spawnAsync(
+      `gcloud auth activate-service-account --key-file ${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
+    );
+    console.log(`Using Google Application Credentials.`);
+  }
 
+  if (prerenderBuildTarget) {
     const run = await context.scheduleTarget(
       targetFromTargetString(prerenderBuildTarget.name),
       prerenderBuildTarget.options
     );
     await run.result;
-
   } else {
-
     if (!context.target) {
       throw new Error('Cannot execute the build target');
     }
@@ -411,17 +498,23 @@ export default async function deploy(
     context.logger.info(`📦 Building "${context.target.project}"`);
 
     const builders = [
-      context.scheduleTarget(
-        targetFromTargetString(staticBuildTarget.name),
-        staticBuildTarget.options
-      ).then(run => run.result)
+      context
+        .scheduleTarget(
+          targetFromTargetString(staticBuildTarget.name),
+          staticBuildTarget.options
+        )
+        .then((run) => run.result),
     ];
 
     if (serverBuildTarget) {
-      builders.push(context.scheduleTarget(
-        targetFromTargetString(serverBuildTarget.name),
-        serverBuildTarget.options
-      ).then(run => run.result));
+      builders.push(
+        context
+          .scheduleTarget(
+            targetFromTargetString(serverBuildTarget.name),
+            serverBuildTarget.options
+          )
+          .then((run) => run.result)
+      );
     }
 
     await Promise.all(builders);
@@ -444,15 +537,21 @@ export default async function deploy(
       const emulator = info[tripleBeam.SPLAT as any]?.[1]?.metadata?.emulator;
       const text = info[tripleBeam.SPLAT as any]?.[0];
       if (text?.replace) {
-        const plainText = text.replace(/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]/g, '');
-        if (emulator?.name === 'hosting' && plainText.startsWith('Local server: ')) {
+        const plainText = text.replace(
+          /\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]/g,
+          ''
+        );
+        if (
+          emulator?.name === 'hosting' &&
+          plainText.startsWith('Local server: ')
+        ) {
           open(plainText.split(': ')[1]);
         }
       }
       return [info.message, ...(info[tripleBeam.SPLAT as any] || [])]
         .filter((chunk) => typeof chunk === 'string')
         .join(' ');
-    })
+    }),
   });
 
   firebaseTools.logger.logger.add(logger);
@@ -466,7 +565,7 @@ export default async function deploy(
         staticBuildTarget,
         serverBuildTarget,
         options,
-        firebaseToken,
+        firebaseToken
       );
     } else {
       await deployToFunction(
@@ -476,7 +575,7 @@ export default async function deploy(
         staticBuildTarget,
         serverBuildTarget,
         options,
-        firebaseToken,
+        firebaseToken
       );
     }
   } else {
@@ -485,8 +584,7 @@ export default async function deploy(
       context,
       context.workspaceRoot,
       options,
-      firebaseToken,
+      firebaseToken
     );
   }
-
 }
